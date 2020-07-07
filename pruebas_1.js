52afa590b4566b285a8584dd4512ce17261f8b92
StifhBL <stifhlife@gmail.com>
2020-07-07 16:58:16 -0500

    Temporaly Changes

---
+++ pruebas_1.js
@@ -0,0 +1,51 @@
+// declarar variable
+let d;
+// iniciar variable
+d = 12;
+/*
+una constante se declara y se 
+inicia al tiempo 
+y no se puede re nombrar
+*/
+const pi = 3.14;
+// strings
+let palabra = 'hola';
+// booleano
+let respuesta = true;
+// nulo dato nulo
+let h = null;
+// inefinido
+h = undefined;
+// funcion imprimir en consola
+let numero = 5.3;
+console.log(numero);
+let palabra2 = 'hola mundo';
+console.log(palabra);
+let respuesta2 = false;
+console.log(respuesta2);
+/*
+post-inremento - a++
+pre-inremento - ++a
+post-decremento - a--
+pre-decremento - --a
+
+*/
+a = 5;
+console.log(a);
+console.log(a++);
+console.log(a);
+console.log(++a);
+console.log(a);
+// a = a + 3
+a += 3;
+console.log(a);
+a -= 3;
+console.log(a);
+a *= 3;
+console.log(a);
+a /= 3;
+console.log(a);
+a %= 3;
+console.log(a);
+console.log(a.length);
+b = "hello friend";
